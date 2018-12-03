a72b1d68a356f8f7f208c0f4c65c9b0f0aaf1efb
emmadrd912 <emma.durand@ynov.com>
2018-12-03 19:15:42 +0100

    fenetre avec balle sans rebond

--- pong.py
+++ pong.py
@@ -1,8 +1,14 @@
-from tkinter import * 
+from tkinter import *
 
+def jeupong():
+    canvas.move(ball, 3, 3)
+    fenetre.after(10, jeupong)
+    
 fenetre = Tk()
+canvas = Canvas(fenetre, width=1080, height=720)
+canvas.pack()
+ball = canvas.create_oval(50,50,100,100)
 
-label = Label(fenetre, text="Hello World")
-label.pack()
+jeupong()
 
 fenetre.mainloop()
