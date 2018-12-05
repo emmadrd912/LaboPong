b831c53b8c6b3b3461714542e85e4e22a8ac2a2e
emmadrd912 <emma.durand@ynov.com>
2018-12-05 09:53:51 +0100

    mise à jour

--- pong.py
+++ pong.py
@@ -1,14 +1,22 @@
 from tkinter import *
 
-def jeupong():
-    canvas.move(ball, 3, 3)
-    fenetre.after(10, jeupong)
-    
+def deplacement():
+    global dx, dy
+    if canvas.coords(ball)[3]>400:
+        dy=-1*dy
+    canvas.move(ball,dx,dy)
+    fenetre.after(20,deplacement)
+
+dx = 0
+dy = 5
+Pos_X=60
+Pos_Y=10
+
 fenetre = Tk()
 canvas = Canvas(fenetre, width=1080, height=720)
-canvas.pack()
-ball = canvas.create_oval(50,50,100,100)
+canvas.pack(padx=10, pady=10)
+ball = canvas.create_oval(Pos_X,Pos_Y,Pos_X+20,Pos_Y+20, fill='red')
 
-jeupong()
+deplacement()
 
 fenetre.mainloop()
