58f159b09bfdd05ca8638c9f679cea2842f0b65b
emmadrd912 <emma.durand@ynov.com>
2018-12-05 11:44:07 +0100

    essaie de faire rebondir

--- pong.py
+++ pong.py
@@ -1,33 +1,41 @@
 from tkinter import *
 
 def deplacement():
-    global dx, dy
-    canvas.move(ball,dx,dy)
-    fenetre.after(20,deplacement)
+    global Pos_X, Pos_Y
+    canvas.move(ball, Pos_X, Pos_Y)
+    fenetre.after(20, deplacement)
 
 def bas1(event):
-    canvas.move(raquette, 0, 10)
+    canvas.move(raquette, 0, 20)
 
 def bas2(event):
-    canvas.move(raquette2, 0, 10)
+    canvas.move(raquette2, 0, 20)
 
 def haut1(event):
-    canvas.move(raquette, 0, -10)
+    canvas.move(raquette, 0, -20)
 
 def haut2(event):
-    canvas.move(raquette2, 0, -10)
+    canvas.move(raquette2, 0, -20)
 
-dx = 0
-dy = 5
-Pos_X=60
-Pos_Y=10
+raqx0 = 70
+raqy0 = 300
+raqx1 = 60
+raqy1 = 420
+
+raq2x0 = 1010
+raq2y0 = 300
+raq2x1 = 1020
+raq2y1 = 420
+
+Pos_X = 500
+Pos_Y = 300
 
 fenetre = Tk()
 canvas = Canvas(fenetre, width=1080, height=720, bg='black')
 canvas.pack(padx=10, pady=10)
 ball = canvas.create_oval(Pos_X, Pos_Y, Pos_X+20, Pos_Y+20, fill='white')
-raquette = canvas.create_rectangle(70, 300, 60, 420, fill='white')
-raquette2 = canvas.create_rectangle(1010, 300, 1020, 420, fill='white')
+raquette = canvas.create_rectangle(raqx0, raqy0, raqx1, raqy1, fill='white')
+raquette2 = canvas.create_rectangle(raq2x0, raq2y0, raq2x1, raq2y1, fill='white')
 
 canvas.bind_all('<s>', bas1)
 canvas.bind_all('<Down>', bas2)
